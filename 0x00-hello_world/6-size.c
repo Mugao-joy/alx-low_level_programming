72f0208a6c74ed7695016f61e692afccde9e5dcd
Mugao-joy <mugaojoy@gmail.com>
2023-04-20 11:28:26 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point 
+ *Description: a script that  prints the size of various types on the computer it is compiled and run on.
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	printf("size of  a char: %lu byte(s)\n",size of (char));
+	printf("size of int: %lu byte(s)\n",size of (int));
+	printf("size of float: %lu byte(s)\n",size of (float));
+	return (0);
+}
