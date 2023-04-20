c2dd038979276edc5e77d475dfe8f3edc802d3c7
Mugao-joy <mugaojoy@gmail.com>
2023-04-20 13:03:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program to print with putchar
+ * main Entry point
  * return: 1 (success)
  */
-int main (void)
+int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
