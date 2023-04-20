dd8fe457b8b733ba6b531bded1a51a02bcf0db85
Mugao-joy <mugaojoy@gmail.com>
2023-04-20 10:47:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle);
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts fuction
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
