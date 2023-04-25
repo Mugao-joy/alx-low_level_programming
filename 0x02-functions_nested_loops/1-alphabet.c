093288a76fcf83f0300cd88bf9cc41f583069c6f
Mugao-joy <mugaojoy@gmail.com>
2023-04-25 04:44:27 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Return: ALways 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
