b7c165555dddf35d56ab88c43f5a47e0495dbbad
Mugao-joy <mugaojoy@gmail.com>
2023-05-16 03:33:34 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int i, ci;
+	int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -18,8 +18,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-		i = j = 0;
-
+	i = j = 0;
 	while (s1[i] != '\0')
 		i++;
 
