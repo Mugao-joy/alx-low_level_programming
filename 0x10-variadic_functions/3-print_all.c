164a5e53788ca90c5aea89240af39c4b76029719
Mugao-joy <mugaojoy@gmail.com>
2023-05-25 10:08:59 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,7 +5,7 @@
  * print_all - function that prints anything
  * @format: list of types of arguments passed to the function
  * @...: standard for va_functions
- * Return: anything
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
@@ -23,14 +23,14 @@ void print_all(const char * const format, ...)
 			switch (format[a])
 			{
 				case 'c':
-					printf("%s%c", seprt, va_arg(args, int));
-					break;
+				printf("%s%c", seprt, va_arg(args, int));
+				break;
 				case 'i':
-					printf("%s%d", seprt, va_arg(args, int));
-					break;
+				printf("%s%d", seprt, va_arg(args, int));
+				break;
 				case 'f':
-					printf("%s%f", seprt, va_arg(args, double));
-					break;
+				printf("%s%f", seprt, va_arg(args, double));
+				break;
 				case 's':
 					s = va_arg(args, char *);
 					if (!s)
